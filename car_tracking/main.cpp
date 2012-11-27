b16f82ebc36f36c8ea36c19c5b8dbc7f9bc031b3
meijia <apple@meijia.local>
2012-11-27 20:46:11 +0800

    Initial Commit

---
+++ car_tracking/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  car_tracking
+//
+//  Created by meijia on 12-11-27.
+//  Copyright (c) 2012年 meijia. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
